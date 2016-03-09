6e070cbeafe974979542521d3b640d0ec2d18cf5
ddcdennisb <dennis@dealertrack.com>
2016-03-08 20:25:43 -0600

    Update and rename Create.CasperSVC.sh to Create.Hidden.Account.sh

--- Create.Hidden.Account.sh
+++ Create.Hidden.Account.sh
@@ -2,11 +2,11 @@
 
 # Create user record in directory services
 dscl . -create /Users/caspersvc
-dscl . -create /Users/caspersvc RealName "caspersvc"
+dscl . -create /Users/caspersvc RealName "USERNAME"
 dscl . -create /Users/caspersvc UniqueID 80  # Use something between 100 and 500 to hide the user
 dscl . -create /Users/caspersvc PrimaryGroupID 0
 dscl . -create /Users/caspersvc UserShell /bin/bash
-dscl . -passwd /Users/caspersvc "Fri3ndlyGh0st"  # Obviously, use something else here
+dscl . -passwd /Users/caspersvc "PASSWORD"  # Obviously, use something else here
 
 # Set up a hidden home folder
 dscl . -create /Users/caspersvc NFSHomeDirectory /private/var/caspersvc  # or other hidden location
@@ -15,4 +15,4 @@ chown -R caspersvc:wheel /private/var/caspersvc
 
 # Grant admin & ARD rights
 dseditgroup -o edit -a caspersvc -t user admin
-/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users administrator,daladmin,admin,casperscreensharing,caspersvc -privs -all -restart -agent
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users UNAMES -privs -all -restart -agent
