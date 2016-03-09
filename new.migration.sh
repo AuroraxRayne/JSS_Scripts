0af89455c68164a46c322b274622ac644a088995
ddcdennisb <dennis@dealertrack.com>
2016-03-08 20:45:48 -0600

    Update new.migration.sh

--- new.migration.sh
+++ new.migration.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-echo "Unjoining Dealerdotcom.corp"
+echo "Unjoining Old Domain"
 
-dsconfigad -remove -u adjoin -p J0inP@ssWrd
+dsconfigad -remove -u UNAME -p PASSWORD
 
 sleep 3
 
 un=`/usr/bin/osascript <<EOT
 tell application "System Events"
     activate
-    set un to text returned of (display dialog "Please type DDC Username" default answer "" with icon 2)
+    set un to text returned of (display dialog "Please type Username" default answer "" with icon 2)
 end tell
 EOT`
 
 domain1=$(dsconfigad -show | grep 'Active Directory Domain')
 
-if [[ ${domain1} =~ 'dealerdotcom.corp' ]]; then
-	echo "Still on Dealerdotcom.corp"
-	dsconfigad -force -remove -u adjoin -p J0inP@ssWrd
+if [[ ${domain1} =~ 'OLD_DOMAIN' ]]; then
+	echo "Still on OLD_DOMAIN"
+	dsconfigad -force -remove -u UNAME -p PASSWORD
 else
-	echo "Looks like unjoin worked.  Lets join the DT Domain"
-	/usr/local/jamf/bin/jamf policy -trigger newvtadbind
+	echo "Looks like unjoin worked.  Lets join the NEW_DOMAIN"
+	/usr/local/jamf/bin/jamf policy -trigger NEW_DOMAIN_POLICY
 	sleep 5
 
 
@@ -39,10 +39,8 @@ echo "results of first check $domain2"
 
 sleep 3
 
-if [[ ${domain2} =~ 'dt.inc' ]]; then
-	echo "Sweet you are on DT.inc You are all set"
-	/usr/local/jamf/bin/jamf -trigger policybanner
-	sleep 2
+if [[ ${domain2} =~ 'NEW_DOMAIN' ]]; then
+	echo "Sweet you are on NEW_DOMAIN You are all set"
 	chown -R $un /Users/$un
 	sleep 2
 prompt1=`/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -icon /System/Library/CoreServices/Installer.app/Contents/Resources/Installer.icns -heading "Migration Complete" -description "The Migration has completed successfully.  Once you click OK your computer will reboot in 1 minute" -button1 "OK"`
