d222e27c0a8cbc6360ac848a1b3a9861326dc079
ddcdennisb <dennis@dealertrack.com>
2016-05-05 14:51:57 -0400

    Update Unjoin.sh

--- Unjoin.sh
+++ Unjoin.sh
@@ -2,5 +2,19 @@
 
 dsconfigad -force -remove -u UNAME -p PASSWORD
 
-echo "Computer has been unjoined"
+echo "first check after bind"
+domain=$(dsconfigad -show | grep 'Active Directory Domain')
 
+echo "results of first check $domain"
+
+sleep 3
+
+if [[ ${domain} == '' ]]; then
+	echo "Sweet you are off DT.inc You are all set"
+	sleep 2
+prompt1=`/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -icon /System/Library/CoreServices/Installer.app/Contents/Resources/Installer.icns -heading "Disjoin Complete" -description "The disjoin from DT.INC has completed successfully." -button1 "OK"`
+    	
+else
+	prompt3=`/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -icon /System/Library/CoreServices/Installer.app/Contents/Resources/Installer.icns -heading "Disjoin Failed" -description "The disjoin form DT.INC has failed. Please run the disjoin manually.  If you continue to have issues reach out to Dennis Browning." -button1 "OK"`
+
+fi
