542da7860a751db91489fe591d121a11337725f0
ddcdennisb <dennis@dealertrack.com>
2016-05-19 10:18:57 -0400

    Update FV.Recovery.Key.sh

--- FV.Recovery.Key.sh
+++ FV.Recovery.Key.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-####################################################################################################
-#
-# Copyright (c) 2013, JAMF Software, LLC.  All rights reserved.
-#
-#       Redistribution and use in source and binary forms, with or without
-#       modification, are permitted provided that the following conditions are met:
-#               * Redistributions of source code must retain the above copyright
-#                 notice, this list of conditions and the following disclaimer.
-#               * Redistributions in binary form must reproduce the above copyright
-#                 notice, this list of conditions and the following disclaimer in the
-#                 documentation and/or other materials provided with the distribution.
-#               * Neither the name of the JAMF Software, LLC nor the
-#                 names of its contributors may be used to endorse or promote products
-#                 derived from this software without specific prior written permission.
-#
-#       THIS SOFTWARE IS PROVIDED BY JAMF SOFTWARE, LLC "AS IS" AND ANY
-#       EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#       WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#       DISCLAIMED. IN NO EVENT SHALL JAMF SOFTWARE, LLC BE LIABLE FOR ANY
-#       DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#       (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#       LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-#       ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#       SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-####################################################################################################
-#
 # Description
 #
 #	The purpose of this script is to allow a new individual recovery key to be issued
@@ -40,18 +12,7 @@
 #	This script will prompt the user for their password so a new FV2 individual
 #	recovery key can be issued and redirected to the JSS.
 #
-####################################################################################################
-# 
-# HISTORY
-#
-#	-Created by Sam Fortuna on Sept. 5, 2014
-#	-Updated by Sam Fortuna on Nov. 18, 2014
-#		-Added support for 10.10
-#   	-Updated by Sam Fortuna on June 23, 2015
-#       	-Properly escapes special characters in user passwords
-#
-####################################################################################################
-#
+
 ## Get the logged in user's name
 userName=$(/usr/bin/stat -f%Su /dev/console)
 
