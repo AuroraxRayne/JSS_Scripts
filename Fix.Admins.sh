012024ccb5cb3868e593475fbd5b7d36397c654d
ddcdennisb <dennis@dealertrack.com>
2016-05-19 10:21:16 -0400

    Update Fix.Admins.sh

--- Fix.Admins.sh
+++ Fix.Admins.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users administrator,admin,casperscreensharing,caspersvc -privs -all -restart -agent
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users USER1,USER2,casperscreensharing,USER3 -privs -all -restart -agent
