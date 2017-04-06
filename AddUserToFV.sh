2293851e059afae7d45a60c0c32387361d32fc0e
Dennis Browning <dennisbrowning@gmail.com>
2017-04-06 08:05:58 -0400

    Update

--- AddUserToFV.sh
+++ AddUserToFV.sh
@@ -218,3 +218,16 @@ fi
 
 exit $FDESETUP_RESULT
 }
+
+
+# Display a branded prompt explaining the password prompt.
+echo "Alerting user $CURRENT_USER about incoming password prompt..."
+prompt=$("$jamfHelper" -windowType "utility" -icon "$LOGO_PNG" -title "$PROMPT_TITLE" -description "$PROMPT_MESSAGE" -timeout 300 -button1 "Next" -defaultButton "2")
+
+if [ $"prompt" == "0" ]; then
+	echo "Lets run addUser"
+	addUser
+else
+	echo "Timer ran out"
+	exit 1
+fi
