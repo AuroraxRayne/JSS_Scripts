a17ad0189e0efaa10dc9687a59f404ac4068d514
Dennis Browning <dennisbrowning@gmail.com>
2017-10-30 13:20:02 -0400

    Update

--- UpgradeLicense_Popup.sh
+++ UpgradeLicense_Popup.sh
@@ -1,34 +1,28 @@
 #!/bin/sh
 jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
-jamfHelperIcon="/System/Library/CoreServices/Installer.app/Contents/Resources/Installer.icns"
+jamfHelperIcon="/Applications/Microsoft Outlook.app/Contents/Resources/ol_Outlook_Email_Hover@2x.png"
 LoggedInUser=`ls -l /dev/console | cut -d " " -f4`
 
 
 fRunUpgrade ()
-{
-    ## In case we need the process ID for the jamfHelper
-	OS_MINOR=$(sw_vers -productVersion | awk -F . '{print $2}')
-	if [[ "$OS_MINOR" -le 9 ]]; then
-	    echo "Lets run the Upgrade to Sierra 10-"
-		/usr/local/jamf/bin/jamf policy -trigger #upgradeToSierra9
-	else
-		echo "Lets run the upgrade to Sierra 10+"
-		/usr/local/jamf/bin/jamf policy -trigger #upgradeToSierra10
-	fi
-    
-    exit 0
+{    
+	echo "Lets run the License upgrade tool"
+	/usr/local/jamf/bin/jamf policy -trigger #2016VolRemoval
+	exit 0
 }
 
 if [ "$LoggedInUser" == "" ]; then
     fRunUpgrade
 else
-    HELPER=$("$jamfHelper" -windowType utility -icon "$jamfHelperIcon" -heading "OS Upgrade to Support Office 2016 and Skype for Business" -description "Did you know that you can upgrade your Operating System so that you can run the latest version of Office 2016 and Skype for Business for Mac?
+    HELPER=$("$jamfHelper" -windowType utility -icon "$jamfHelperIcon" -heading "Upgrade to O365 Subscription" -description "It has been determined that your Office 2016 installation is still using an old license model.  To start the upgrade process, close out of all Microsoft Applications and then click the \"Upgrade\" button below to start the process of upgrading to your subscription license.
 	
-To start the upgrade process, close all open applications and then click the \"Install\" button below.  If you do not want to do it now, you can go into Self Service and run the \"Upgrade to Sierra\" option found on the main page.
+Once complete, Outlook will open and you will be required to login to activate your Office365 Subscription.
+
+If now is not a good time, you can open \"Self Service\" and run the \"2016 Vol Removal\" tool.
 
 **Please note that after January 8th 2018, this update will be forced.**
 
-" -button1 "Install" -button2 "Cancel" -defaultButton "2" -cancelButton "2" -timeout 900 -countdown -alignCountdown center -alignDescription justified)
+" -button1 "Upgrade" -button2 "Cancel" -defaultButton "2" -cancelButton "2" -timeout 900 -countdown -alignCountdown center -alignDescription justified)
 
 
        echo "jamf helper result was $HELPER";
