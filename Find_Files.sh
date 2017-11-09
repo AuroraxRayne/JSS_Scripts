1bfaddf146482bee68ac4fb60756cc99244ffb91
Dennis Browning <dennisbrowning@gmail.com>
2017-11-09 15:22:25 -0500

    Update

--- Find_Files.sh
+++ Find_Files.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+#touch .netrc
+#chmod 700
+
+#echo "machine ftp.dennisbrowning.me" >> .netrc
+#echo "login test" >> .netrc
+#echo "password Earth123!" >> .netrc
+
 Search1=( $(mdfind 'poidf;lakdf - a;lkndf;d') )
 Search2=( $(mdfind 'poidf;lakdf a;lkndf;d') )
 Search3=( $(mdfind 'apjtak;egt') )
 
-touch /Library/Application\ Support/CAI/Search_Results.txt
-Search_Results=/Library/Application\ Support/CAI/Search_Results.txt
+User=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+
+Now=$(/bin/date +"%m-%d-%Y")
+
+touch /Library/Application\ Support/CAI/Search_Results_"$User"_$Now.txt
+Search_Results=/Library/Application\ Support/CAI/Search_Results_"$User"_$Now.txt
 
 echo "Search results for \"Inventory Solutions – Franchise Order\"" >> $Search_Results
 for file1 in ${Search1[@]}; do
@@ -33,5 +44,9 @@ for file3 in ${Search3[@]}; do
 done
 
 
-
+#ftp -d ftp.dennisbrowning.me << ftpEOF
+#prompt
+#put $Search_Results.txt
+#quit
+#ftpEOF
 
