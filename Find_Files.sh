c74cc723ca1ab0332548182a43c686ee17623ad4
Dennis Browning <dennisbrowning@gmail.com>
2017-11-10 07:57:28 -0500

    Update

--- Find_Files.sh
+++ Find_Files.sh
@@ -7,10 +7,6 @@
 #echo "login test" >> .netrc
 #echo "password Earth123!" >> .netrc
 
-Search1=( $(mdfind 'poidf;lakdf - a;lkndf;d') )
-Search2=( $(mdfind 'poidf;lakdf a;lkndf;d') )
-Search3=( $(mdfind 'apjtak;egt') )
-
 User=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
 Now=$(/bin/date +"%m-%d-%Y")
@@ -18,30 +14,40 @@ Now=$(/bin/date +"%m-%d-%Y")
 touch /Library/Application\ Support/CAI/Search_Results_"$User"_$Now.txt
 Search_Results=/Library/Application\ Support/CAI/Search_Results_"$User"_$Now.txt
 
+OLD_IFS=$IFS
+IFS=$'\n'
+
 echo "Search results for \"Inventory Solutions – Franchise Order\"" >> $Search_Results
-for file1 in ${Search1[@]}; do
-	ls -hal $file1 
+
+for file in $(mdfind 'Inventory Solutions – Franchise Order')
+do
+	ls -hal "$file"
 	if [ $? -eq 0 ]; then
-		echo "$(ls -hal $file1)" >> $Search_Results
+		echo "$(ls -hal "$file")" >> $Search_Results
 	fi
 done
 
 echo "Search results for \"Inventory Solutions Franchise Order\"" >> $Search_Results
-for file2 in ${Search2[@]}; do
-	ls -hal $file2
-	if [ $? -eq 0 ]; then
-	echo "$(ls -hal $file2)" >> $Search_Results
-	fi
 
+for file in $(mdfind 'Inventory Solutions Franchise Order')
+do
+	ls -hal "$file"
+	if [ $? -eq 0 ]; then
+		echo "$(ls -hal "$file")" >> $Search_Results
+	fi
 done
 
 echo "Search results for \"AAX Products Terms and Conditions\"" >> $Search_Results
-for file3 in ${Search3[@]}; do
-	ls -hal $file3
+
+for file in $(mdfind 'AAX Products Terms and Conditions')
+do
+	ls -hal "$file"
 	if [ $? -eq 0 ]; then
-	echo "$(ls -hal $file3)" >> $Search_Results
+		echo "$(ls -hal "$file")" >> $Search_Results
 	fi
 done
+IFS=$OLD_IFS
+
 
 
 #ftp -d ftp.dennisbrowning.me << ftpEOF
