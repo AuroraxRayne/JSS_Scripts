f3780c847e0a865c241ba7645877245ed95497d6
Dennis Browning <dennisbrowning@gmail.com>
2018-02-13 10:30:55 -0500

    update

--- rename.sh
+++ rename.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-#model=`/usr/bin/osascript <<EOT
-#tell application "System Events"
- #   activate
-  #  set model to text returned of (display dialog "Please type 'L' for laptop or 'W' for Desktop" default answer "" with icon 2)
-#end tell
-#EOT`
 model=""
 
 modelName=$(system_profiler SPHardwareDataType | awk '/Model Name/{print $3}')
@@ -19,25 +13,25 @@ else
 	model="w"
 fi
 
-office=`/usr/bin/osascript <<EOT
-tell application "System Events"
-    activate
-    set office to text returned of (display dialog "Please type 3 letter Location Code BUR, DAL, NHP, etc" default answer "" with icon 2)
-end tell
-EOT`
+#office=`/usr/bin/osascript <<EOT
+#tell application "System Events"
+ #   activate
+  #  set office to text returned of (display dialog "Please type 3 letter Location Code BUR, DAL, NHP, etc" default answer "" with icon 2)
+#end tell
+#EOT`
 
 
-OS=Mac
+OS=mac
 
 sn=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 newsn=$(echo "${sn:4}")
 
 
-name=$model$office$OS$newsn
+name=$model$OS$newsn
 
 echo $name
 
-#networksetup -setcomputername $name 
-#scutil --set LocalHostName $name
-#scutil --set HostName $name
-#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName $name
+networksetup -setcomputername $name 
+scutil --set LocalHostName $name
+scutil --set HostName $name
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName $name
