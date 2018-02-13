bb75b849e48e2055426c1ba872ea7b106bba63e0
Dennis Browning <dennisbrowning@gmail.com>
2018-02-13 08:43:06 -0500

    Updated

--- rename.sh
+++ rename.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-model=`/usr/bin/osascript <<EOT
-tell application "System Events"
-    activate
-    set model to text returned of (display dialog "Please type 'L' for laptop or 'W' for Desktop" default answer "" with icon 2)
-end tell
-EOT`
+#model=`/usr/bin/osascript <<EOT
+#tell application "System Events"
+ #   activate
+  #  set model to text returned of (display dialog "Please type 'L' for laptop or 'W' for Desktop" default answer "" with icon 2)
+#end tell
+#EOT`
+model=""
+
+modelName=$(system_profiler SPHardwareDataType | awk '/Model Name/{print $NF}')
+
+if [[ "$modelName" =~ "Book" ]]; then
+	echo "Looks to be a laptop"
+	model="l"
+else
+	echo "Looks to be a desktop"
+	model="w"
+fi
 
 office=`/usr/bin/osascript <<EOT
 tell application "System Events"
@@ -25,7 +36,7 @@ name=$model$office$OS$newsn
 
 echo $name
 
-networksetup -setcomputername $name 
-scutil --set LocalHostName $name
-scutil --set HostName $name
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName $name
+#networksetup -setcomputername $name 
+#scutil --set LocalHostName $name
+#scutil --set HostName $name
+#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName $name
