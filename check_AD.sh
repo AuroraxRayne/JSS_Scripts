0103bdf41d75686da23b63d50875b4d64b573e56
Dennis Browning <dennisbrowning@gmail.com>
2018-03-02 14:48:16 -0500

    Update

--- check_AD.sh
+++ check_AD.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 
 #check to be sure computer is bound to AD
-domainCheck=$(dsconfigad -show | grep 'Active Directory Domain')
+domainCheck=$(dsconfigad -show | awk '/Active Directory Domain/{print $NF}')
 
-if [[ ${domainCheck} == "" ]]; then
-	echo "This computer is not joined to a domain"
-	exit 1
-fi
-
+runCheck ()
+{
 #Setting Variables to compare later on
-mobileEnabled=$(dsconfigad -show | grep "Create mobile" | awk '{print $7}')
+mobileEnabled=$(dsconfigad -show | awk '/Create mobile account at login/{print $NF}')
 
 echo "Create mobile account is: $mobileEnabled"
 
-confirmationEnabled=$(dsconfigad -show | grep "Require" | awk '{print $4}')
+confirmationEnabled=$(dsconfigad -show | awk '/Require confirmation/{print $NF}')
 
 echo "Require confirmation is: $confirmationEnabled"
 
-
 #Check to see if Create mobile account at login is enabled.  If so, exit out.  If not, set it to enable and disable confirmation
-if [[ $mobileEnabled == "Enabled" ]]; then
+if [[ ${mobileEnabled} == Enabled ]]; then
 	echo "Mobile account enabled"
 	exit 0
 else
@@ -28,10 +24,22 @@ else
 fi
 
 #Recheck to confirm new settings are set
-mobileEnabledCheck=$(dsconfigad -show | grep "Create mobile" | awk '{print $7}')
+mobileEnabledCheck=$(dsconfigad -show | awk '/Create mobile account at login/{print $NF}')
 
 echo "Checking Create mobile account is: $mobileEnabledCheck"
 
-confirmationEnabledCheck=$(dsconfigad -show | grep "Require" | awk '{print $4}')
+confirmationEnabledCheck=$(dsconfigad -show | awk '/Require confirmation/{print $NF}')
 
 echo "Checking Require confirmation is: $confirmationEnabledCheck"
+}
+
+
+if [[ ${domainCheck} == man.co ]]; then
+	echo "Sweet this computer is joined to man.co"
+	runCheck
+	exit 0
+else
+    echo "Computer is not joined"
+	/usr/local/bin/jamf policy -trigger joinmanco
+    runCheck
+fi
