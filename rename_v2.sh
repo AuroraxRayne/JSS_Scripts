8755ae25988947fcd147732c714ca16969c1ae5e
Dennis Browning <dennisbrowning@gmail.com>
2018-05-09 10:23:58 -0400

    Update

--- rename_v2.sh
+++ rename_v2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-osascript <<EOT
+nameCode=`/usr/bin/osascript <<EOT
 
 set locations to {"Main Campus Atlanta", "Summit Campus Atlanta", "Burlington", "North Hills", "Dallas", "Austin", "Groton", "Mississauga", "South Jordan", "HomeNet", "Vin Solutions", "NextGear Capital", "Ready Logistics", "KBB Irvine", "xTime", "Other"}
 
@@ -9,52 +9,54 @@ set city to the result
 set code to city as text
 
 if (code = "Main Campus Atlanta") then
-	set nameCode to "man"
+	set nameCode to "MAN"
 else if (code = "Summit Campus Atlanta") then
-	set nameCode to "atl"
+	set nameCode to "ATL"
 else if (code = "Burlington") then
-	set nameCode to "bur"
+	set nameCode to "BUR"
 else if (code = "North Hills") then
-	set nameCode to "nhp"
+	set nameCode to "NHP"
 else if (code = "Dallas") then
-	set nameCode to "dal"
+	set nameCode to "DAL"
 else if (code = "Austin") then
-	set nameCode to "vat"
+	set nameCode to "VAT"
 else if (code = "Groton") then
-	set nameCode to "gro"
+	set nameCode to "GRO"
 else if (code = "Mississauga") then
-	set nameCode to "mis"
+	set nameCode to "MIS"
 else if (code = "South Jordan") then
-	set nameCode to "slc"
+	set nameCode to "SLC"
 else if (code = "HomeNet") then
-	set nameCode to "hmn"
+	set nameCode to "HMN"
 else if (code = "Vin Solutions") then
-	set nameCode to "vin"
+	set nameCode to "VIN"
 else if (code = "NextGear Capital") then
-	set nameCode to "ngc"
+	set nameCode to "NGC"
 else if (code = "Ready Logistics") then
-	set nameCode to "rdl"
+	set nameCode to "RDL"
 else if (code = "KBB Irvine") then
-	set nameCode to "kbb"
+	set nameCode to "KBB"
 else if (code = "xTime") then
-	set nameCode to "rwc"
+	set nameCode to "RWC"
 else
-	set nameCode to "cai"
+	set nameCode to "CAI"
 end if
 
+EOT`
 
-set sn to (do shell script ("system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | cut -c 5-"))
-set model to (do shell script ("system_profiler SPHardwareDataType | awk '/Model Name/ {print $3}'"))
-if (model = "MacBook") then
-	set modelCode to "L"
+sn=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | cut -c 5-)
+model=$(system_profiler SPHardwareDataType | awk '/Model Name/ {print $3}')
+
+if [[ model == "MacBook" ]]; then
+	modelCode="L"
 else
-	set modelCode to "W"
-end if
+	modelCode="W"
+fi
+os="mac"
 
-set computerName to modelCode & nameCode & "MAC" & sn
+computerName=$modelCode$nameCode$os$sn
 
-do shell script "networksetup -setcomputername " & computerName
-do shell script "scutil --set LocalHostName " & computerName
-do shell script "scutil --set HostName " & computerName
-do shell script "defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName " & computerName 
-EOT
+networksetup -setcomputername $computerName
+scutil --set LocalHostName $computerName
+scutil --set HostName $computerName
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName $computerName
