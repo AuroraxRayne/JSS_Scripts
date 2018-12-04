adbe3ea0d4bdd9d1438df66920dd5ba53578b5c8
Dennis Browning <dennisbrowning@gmail.com>
2018-12-04 15:26:04 -0500

    added into message

--- selfCollection.sh
+++ selfCollection.sh
@@ -8,6 +8,10 @@ systemsetup -setcomputersleep Never
 User=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
 outputFolder=""
+echo "CEI Self Collection tool"
+echo "Please disregard any messages that say \"Operation not permitted\""
+read -p "Press any key to continue... " -n1 -s
+
 
 echo "Calculating Folder size...."
 userHomeDirSize=$(du -sh /Users/$User | awk '{print $1}')
